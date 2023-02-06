64714b420b9761ac7d9e9fbf4bacef142417a74b
Omar Noyola <omar.noyolam@gmail.com>
2023-02-06 14:05:25 -0600

    :white_check_mark: Cambio de servidor

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', 'dpg-cf8nu2irrk0e2au6ajgg-a.oregon-postgres.render.com'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'ceffan'),
-      user: env('PGUSER', 'root'),
-      password: env('PGPASSWORD', '2x9wnolAQj5vknhC1K3NH6TZY7pqHCLa'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('PGHOST'),
+      port: env.int('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
