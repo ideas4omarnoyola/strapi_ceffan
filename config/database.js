963e7419d5a1503b9f959e5d688a33489c11b78d
Omar Noyola <omar.noyolam@gmail.com>
2023-02-06 11:50:30 -0600

    :zap: Ajuste conexión DB

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cf8nu2irrk0e2au6ajgg-a.oregon-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ceffan'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', '2x9wnolAQj5vknhC1K3NH6TZY7pqHCLa'),
+      host: env('PGHOST', 'dpg-cf8nu2irrk0e2au6ajgg-a.oregon-postgres.render.com'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'ceffan'),
+      user: env('PGUSER', 'root'),
+      password: env('PGPASSWORD', '2x9wnolAQj5vknhC1K3NH6TZY7pqHCLa'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
